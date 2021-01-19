40755b48974c985d0bf5d1ff10aeb2e683a56356
Joe Buono <joebuono724@gmail.com>
2021-01-19 11:39:51 -0500

    LLcycle

--- green/linkedListCycle.js
+++ green/linkedListCycle.js
@@ -50,6 +50,14 @@ Follow up: Can you solve it using O(1) (i.e. constant) memory?
  * @return {boolean}
  */
 var hasCycle = function(head) {
-  
-};
+  let slow = head;
+  let fast = head;
 
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+
+  return false;
+};
