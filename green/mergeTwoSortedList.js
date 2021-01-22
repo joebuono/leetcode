e9cc91720ea8c7ea9976ce06b862e9bd8d1885dd
Joe Buono <joebuono724@gmail.com>
2021-01-22 12:55:21 -0500

    merge two sorted linked lists

--- green/mergeTwoSortedList.js
+++ green/mergeTwoSortedList.js
@@ -73,7 +73,27 @@ E:
 
 Two pointers
 
-
-
-
 */
+
+
+
+// 1/22/21
+var mergeTwoLists = function(l1, l2) {
+  let dummyHead = new ListNode();
+  let current = dummyHead;
+
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      current.next = l1;
+      l1 = l1.next;
+    } else {
+      current.next = l2;
+      l2 = l2.next;
+    }
+    current = current.next;
+  }
+
+  current.next = l1 ? l1 : l2;
+
+  return dummyHead.next;
+};
