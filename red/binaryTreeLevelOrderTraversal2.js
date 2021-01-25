85b95c47f321e36d5905e2f0a4102cb6bf8b762d
Joe Buono <joebuono724@gmail.com>
2021-01-25 11:30:56 -0500

    binary tree level order traverssal ii

---
+++ red/binaryTreeLevelOrderTraversal2.js
@@ -0,0 +1,58 @@
+/*
+
+Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its bottom-up level order traversal as:
+[
+  [15,7],
+  [9,20],
+  [3]
+]
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+  if (!root) return [];
+
+  let result = [];
+  // use a queue and do BFS
+  let queue = [root];
+
+  while (queue.length) {
+    let currentLevel = [];
+    let size = queue.length;
+    for (let i = 0; i < size; i++) {
+      let currentNode = queue.shift();
+      currentLevel.push(currentNode.val);
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
+    result.unshift(currentLevel);
+  }
+
+  return result;
+};
+
