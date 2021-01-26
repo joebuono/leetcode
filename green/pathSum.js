5e2da840414eea5f9ea222753e6b9b2c1acf59e4
Joe Buono <joebuono724@gmail.com>
2021-01-26 12:32:25 -0500

    pathSum

---
+++ green/pathSum.js
@@ -0,0 +1,76 @@
+/*
+
+Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf path such that adding up all the values along the path equals targetSum.
+
+A leaf is a node with no children.
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22
+Output: true
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: false
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+
+// Elegant recursive solution
+var hasPathSum = function(root, targetSum) {
+  if (!root) {
+    return false;
+  } else if (!root.left && !root.right && targetSum - root.val === 0) {
+    return true;
+  } else {
+    return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+  }
+}
+
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+  if (!root) return false;
+  var pathSum = false;
+
+  var dfs = function(node, total) {
+    var newTotal = node.val + total;
+    if (newTotal === targetSum && !node.left && !node.right) {
+      pathSum = true;
+    } else {
+      if (node.left) dfs(node.left, newTotal);
+      if (node.right) dfs(node.right, newTotal);
+    }
+  };
+
+  dfs(root, 0);
+
+  return pathSum;
+};
+
+
