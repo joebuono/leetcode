be9d6048e086ea9bb2635736ba3d7bf7e6a16846
Joe Buono <joebuono724@gmail.com>
2021-02-01 17:50:05 -0500

    subsets ii

--- subsetsII.js
+++ subsetsII.js
@@ -27,7 +27,33 @@ Constraints:
  * @param {number[]} nums
  * @return {number[][]}
  */
+
+
+// Beautiful solution
 var subsetsWithDup = function(nums) {
-    
+  // First, sort the numbers. This will allow us to check for duplicates
+  nums.sort((a, b) => a - b);
+  let powerSet = [];
+
+  var permute = function(perm, index) {
+    // Push a copy of the perm (due to pass by reference)
+    powerSet.push([...perm]);
+    for (let i = index; i < nums.length; i++) {
+      // If we haven't just started this recursive call
+      // and if the element isn't the same as the previous element
+      // i !== index is critical
+      if (i !== index && nums[i] === nums[i - 1]) {
+        continue;   
+      }
+      // Push before the recursive call, and pop after the recursive call
+      perm.push(nums[i]);
+      permute(perm, i + 1);
+      perm.pop();
+    }
+  };
+
+  permute([], 0);
+
+  return powerSet;
 };
 
