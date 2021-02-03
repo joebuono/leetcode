5b4ad01340da38a77b8c09075c36fc6e6bddacb6
Joe Buono <joebuono724@gmail.com>
2021-02-02 22:04:34 -0500

    house robber, no problem

--- green/houseRobber.js
+++ green/houseRobber.js
@@ -67,3 +67,21 @@ We want to calculate the "best so far" at every point
 Math.max(previous, current + skipBack)
 
 */
+
+// 2/2/21
+var rob = function(nums) {
+  if (!nums.length) return 0;
+  if (nums.length === 1) return nums[0];
+  if (nums.length === 2) return Math.max(nums[0], nums[1]);
+
+  let dp = new Array(nums.length);
+  dp[0] = nums[0];
+  dp[1] = Math.max(nums[0], nums[1]);
+
+  for (let i = 2; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+  }
+
+  return dp[nums.length - 1];
+};
+
