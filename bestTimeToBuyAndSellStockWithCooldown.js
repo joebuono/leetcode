df37a0fc55f2f9ac852a147a82a9ae9c34ca7886
Joe Buono <joebuono724@gmail.com>
2021-02-09 22:47:35 -0500

    a cryptic solution to best time to sell stock with cooldown

--- bestTimeToBuyAndSellStockWithCooldown.js
+++ bestTimeToBuyAndSellStockWithCooldown.js
@@ -18,20 +18,70 @@ Explanation: transactions = [buy, sell, cooldown, buy, sell]
  * @param {number[]} prices
  * @return {number}
  */
+
+
+// What in the hell is this?!
+// https://www.youtube.com/watch?v=w6xk5Po-DX0
 var maxProfit = function(prices) {
+  if (prices.length < 2) return 0;
+
+  if (prices.length === 2) {
+    return Math.max(prices[1] - prices[0], 0);
+  }
+
+  let dp = new Array(prices.length).fill([]);
+
+  dp[0][0] = 0;
+  dp[0][1] = prices[0] * -1;
+  dp[1][0] = Math.max(dp[0][0], dp[0][1] + prices[1]);
+  dp[1][1] = Math.max(dp[0][1], dp[0][0] - prices[1]);
+
+  for (let i = 2; i < prices.length; i++) {
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]); 
+  }
+
+  return dp[dp.length - 1][0];
+};
+
+
+
+
+
+// This raw solution doesn't work
+var maxProfit = function(prices) {
+  if (prices.length < 2) return 0;
+  
   let profits = new Array(prices.length).fill(0);
   profits[1] = Math.max(prices[1] - prices[0], 0);
 
+  let minThusFar = Math.min(prices[0], prices[1]);
+  let minThusFarIndex = minThusFar === prices[0] ? 0 : 1;
+
   for (let i = 2; i < prices.length; i++) {
-    let cooldown = i < 3 ? 0 : prices[i - 3];
+    let minMax = 0;
+    if (minThusFarIndex < i - 1) {
+      minMax = prices[i] - minThusFar;
+      if (minThusFarIndex - 2 > 0) {
+        minMax += profits[minThusFarIndex - 2]
+      }
+    }
+
+    let cooldown = i < 3 ? 0 : profits[i - 3];
+    let cooldown2 = i < 4 ? 0 : profits[i - 4];
     profits[i] = Math.max(
       prices[i] - prices[i - 1] + cooldown,
-      prices[i] - prices[i - 2] + cooldown,
-      cooldown
+      prices[i] - prices[i - 2] + cooldown2,
+      profits[i - 1],
+      minMax
     );
+    
+    if (prices[i] < minThusFar) {
+      minThusFar = prices[i];
+      minThusFarIndex = i;
+    }
   }
 
-  return profits[profits.lengths - 1];
+  return profits[profits.length - 1];
 };
-
 
