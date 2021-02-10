e1e89bc6648f8c5777f4f66088ce43fe9d6ffac1
Joe Buono <joebuono724@gmail.com>
2021-02-10 11:31:02 -0500

    add two numbers

---
+++ yellow/addTwoNumbers.js
@@ -0,0 +1,69 @@
+/*
+
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+ 
+
+Constraints:
+
+The number of nodes in each linked list is in the range [1, 100].
+0 <= Node.val <= 9
+It is guaranteed that the list represents a number that does not have leading zeros.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+  let dummyHead = new ListNode();
+  let curr = dummyHead;
+  let carry = 0;
+  let p1 = l1;
+  let p2 = l2;
+
+  while (p1 || p2) {
+    let val1 = p1 ? p1.val : 0;
+    let val2 = p2 ? p2.val : 0;
+    let newVal = val1 + val2 + carry;
+
+    carry = Math.floor(newVal / 10); // 1 or 0
+    curr.next = new ListNode(newVal % 10);
+    curr = curr.next;
+
+    if (p1) p1 = p1.next;
+    if (p2) p2 = p2.next;
+  }
+
+  if (carry) {
+    curr.next = new ListNode(carry);
+  }
+
+  return dummyHead.next;
+};
+
