7ec80a8c3f4b09ef130767d3c360983b872ff471
Joe Buono <joebuono724@gmail.com>
2021-04-02 12:04:33 -0400

    LRU cache

--- LRUcache.js
+++ LRUcache.js
@@ -7,7 +7,7 @@ LRU Cache
 /**
  * @param {number} capacity
  */
- var LRUCache = function(capacity) {
+var LRUCache = function(capacity) {
     
 };
 
@@ -34,3 +34,53 @@ LRUCache.prototype.put = function(key, value) {
  * var param_1 = obj.get(key)
  * obj.put(key,value)
  */
+
+// DLL
+class DLL {
+  constructor(capacity) {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+    this.capacity = capacity;
+  }
+
+  // insert
+  // return node
+  insert(val) {
+    const node = new Node(val);
+    // if no head
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.prev = this.tail;
+      this.tail = node;
+    }
+
+    this.size++;
+    if (this.size > this.capacity) {
+      // pop off lru
+    }
+  }
+
+  // remove
+  // return node
+  remove(node) {
+    // what if it's the head or tail?
+    let before = node.prev;
+    let after = node.next;
+    before.next = after;
+    after.prev = before;
+  }
+}
+
+
+// DLL Node
+class Node {
+  constructor(value) {
+    this.val = value;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
