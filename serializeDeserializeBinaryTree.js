1aea36d508a2e7873866d1dbac6a11dafceb39eb
Joe Buono <joebuono724@gmail.com>
2021-04-04 00:22:57 -0400

    minpathSum" ; ~Q
    sdf;

--- serializeDeserializeBinaryTree.js
+++ serializeDeserializeBinaryTree.js
@@ -1,9 +1,5 @@
-/*
 
-
-
-*/
-
+// Experiment with BFS, and also iterative implementation of DFS preorder
 
 /**
  * Definition for a binary tree node.
@@ -13,61 +9,80 @@
  * }
  */
 
+ function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val)
+  this.left = (left===undefined ? null : left)
+  this.right = (right===undefined ? null : right)
+}
+
 /**
- * Encodes a tree to a single string.
- *
- * @param {TreeNode} root
- * @return {string}
- */
+* Encodes a tree to a single string.
+*
+* @param {TreeNode} root
+* @return {string}
+*/
 var serialize = function(root) {
-  if (!root) return null;
-  let str = '';
+if (!root) return 'n';
+let str = '';
 
-  const preorderDFS = function(node) {
-    if (!node) {
-      str += 'n';
-      return;
-    } 
+const preorderDFS = function(node) {
+  if (!node) {
+    str += 'n,';
+    return;
+  } 
 
-    str += node.val + ',';
-    preorderDFS(node.left);
-    preorderDFS(node.right);
-  };
+  str += node.val + ',';
+  preorderDFS(node.left);
+  preorderDFS(node.right);
+};
 
-  preorderDFS(root);
+preorderDFS(root);
 
-  return str;
+return str;
 };
 
 /**
- * Decodes your encoded data to tree.
- *
- * @param {string} data
- * @return {TreeNode}
- */
+* Decodes your encoded data to tree.
+*
+* @param {string} data
+* @return {TreeNode}
+*/
 var deserialize = function(data) {
-  let arr = data.split(',');
-  let index = 0;
+let arr = data.split(',');
+let index = 0;
 
-  const construct = function() {
-    if (index > arr.length || arr[index] === 'n') {
-      return null;
-    }
-
-    root = new TreeNode(parseInt(arr[index]));
-
+const construct = function() {
+  if (index > arr.length || arr[index] === 'n') {
     index++;
-    root.left = construct();
-    index++;
-    root.right = construct();
+    return null;
+  }
 
-    return root;
-  };
+  const root = new TreeNode(parseInt(arr[index++]));
 
-  return construct();
+  root.left = construct();
+  root.right = construct();
+
+  return root;
 };
 
+return construct();
+};
+
+let five = new TreeNode(5);
+let four = new TreeNode(4);
+let three = new TreeNode(3, four, five);
+let two = new TreeNode(2);
+let one = new TreeNode(1, two, three);
+
+
+let ser = serialize(one);
+console.log(ser);
+
+debugger;
+let deser = deserialize(ser);
+console.log(deser);
+
 /**
- * Your functions will be called as such:
- * deserialize(serialize(root));
- */
+* Your functions will be called as such:
+* deserialize(serialize(root));
+*/
