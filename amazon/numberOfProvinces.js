ad90ae8f2036186b295a00b4fb26e7117f96ce63
Joe Buono <joebuono724@gmail.com>
2021-04-07 18:36:46 -0400

    number of provinces

--- amazon/numberOfProvinces.js
+++ amazon/numberOfProvinces.js
@@ -14,7 +14,60 @@ Return the total number of provinces.
  * @param {number[][]} isConnected
  * @return {number}
  */
+
+// DFS
 var findCircleNum = function(isConnected) {
-    
+  let n = isConnected.length;
+
+  const visited = new Array(n).fill(0);
+  let count = 0;
+
+  for (let i = 0; i < n; i++) {
+    if (visited[i] === 0) {
+      dfs(isConnected, visited, i);
+      count++;
+    }
+  }
+
+  return count;
 };
 
+var dfs = function(M, visited, i) {
+  visited[i] = 1;
+
+  for (let j = 0; j < M.length; j++) {
+    if (M[i][j] === 1 && visited[j] === 0) {
+      dfs(M, visited, j);
+    }
+  }
+};
+
+
+// Basically, it's an adjacency matrix
+// Attempt at Union-Find
+var findCircleNum = function(isConnected) {
+  let n = isConnected.length;
+  const roots = new Array(n);
+  for (let i = 0; i < n; i++) roots[i] = i;
+
+  for (let i = 0; i < n; i++) {
+    let root1 = find(i, roots);
+    for (let j = 0; j < n; j++) {
+      let root2 = find(j, roots);
+      if (isConnected[i][j] && root1 !== root2) {
+        roots[root1] = root2;
+        n--;
+      }
+    }
+  }
+
+  return n;
+};
+
+var find = function(id, roots) {
+  while (roots[id] !== id) {
+    roots[id] = roots[roots[id]]; // path compression
+    id = roots[id];
+  }
+  return id;
+}
