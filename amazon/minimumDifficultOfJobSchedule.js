928f43a323ebcf00c8ee3745a83b9468f9905c5a
Joe Buono <joebuono724@gmail.com>
2021-04-09 15:04:59 -0400

    min diff job schedule

--- amazon/minimumDifficultOfJobSchedule.js
+++ amazon/minimumDifficultOfJobSchedule.js
@@ -20,61 +20,42 @@ Return the minimum difficulty of a job schedule. If you cannot find a schedule f
  // Elegant DP solution
  // https://leetcode.com/problems/minimum-difficulty-of-a-job-schedule/discuss/944828/Short-DP-solution-with-highly-detailed-step-by-step-explanation
 
+// *** Rephrase the problem statement:
+// Given an array, cut it into d contiguous subarrays and return the minimum sum of the max of each subarray
 
 
-// Rough first attempt
- var minDifficulty = function(jobDifficulty, d) {
-  if (d > jobDifficulty.length) return -1;
+var minDifficulty = function(jobDifficulty, d) {
+  if (jobDifficulty.length < d) return -1;
 
-  const windowSize = jobDifficulty - (d - 1);
-  let indexOfMax = 0;
-  let max = jobDifficulty[0];
-  for (let i = 1; i < jobDifficulty.length; i++) {
-    if (jobDifficulty[i] > max) {
-      max = jobDifficulty[i];
-      indexOfMax = i;
-    }
-  }
+  // initialize dp matrix
+  const dp = new Array(d + 1).fill(0).map(() => new Array(jobDifficulty.length).fill(-1));
 
-  // we don't want the start or end indices to be out of bounds
-  let start = Math.max(0, indexOfMax - windowSize + 1);
-  let end = Math.min(jobDifficulty.length, start + windowSize - 1);
-  
-  let maxSum = windowSum;
-  let indices = [start, end];
-  let windowSum = 0;
-  for (let i = start; i <= end; i++) {
-    windowSum += jobDifficulty[i];
-  }
-
-  while (start < indexOfMax && end < jobDifficulty.length - 1) {
-    windowSum -= jobDifficulty[start++];
-    windowSum += jobDifficulty[end++];
-    if (windowSum > maxSum) {
-      maxSum = windowSum;
-      indices = [start, end];
-    }
-  }
-
-  let remainingDays = 0;
-
-  for (let i = 0; i < jobDifficulty.length; i++) {
-    if (i < indices[0] && i > indices[end]) {
-      remainingDays += jobDifficulty[i];
-    }
-  }
-
-  return remainingDays + max; 
-
-  // find the max length a window can be such that it leaves exactly d - 1 days left. For example:
-  // arrLength = 8;
-  // d = 6;
-  // 8 - (6 - 1) = 3
-  // The max length for the window is 3
-
-  // find the largest integer (most difficult task)
-  // from that point, find a window of length maxLength with the largest sum
-  // sum the remaining days that were not included in the window
+  // recurse, checking every possible way to split the input array
+  return dfs(jobDifficulty, d, dp, 0);
 };
 
+var dfs = function(jobDifficulty, d, dp, idx) {
+  // if there's only 1 day left, return the max of the remaining days
+  if (d === 1) {
+    let max = 0;
+    while (idx < jobDifficulty.length) {
+      max = Math.max(max, jobDifficulty[idx++]);
+    };
+    return max;
+  }
 
+  // if we've already calculated the minimum for this day and index, return that memoized result
+  if (dp[d][idx] !== -1) return dp[d][idx];
+
+  // THE MEAT
+  let max = 0;
+  let res = Number.MAX_SAFE_INTEGER;
+
+  for (let i = idx; i < jobDifficulty.length - d + 1; i++) {
+    max = Math.max(max, jobDifficulty[i]);
+    res = Math.min(res, max + dfs(jobDifficulty, d - 1, dp, i + 1));
+  }
+
+  dp[d][idx] = res;
+  return res;
+};
