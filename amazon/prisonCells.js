24ed16b261964cb26ff5b45c50d5ce27111d3bc1
Joe Buono <joebuono724@gmail.com>
2021-04-13 19:14:15 -0400

    prison cells

--- amazon/prisonCells.js
+++ amazon/prisonCells.js
@@ -3,7 +3,40 @@
  * @param {number} n
  * @return {number[]}
  */
- var prisonAfterNDays = function(cells, n) {
-    
+var prisonAfterNDays = function(cells, n) {
+  const cache = new Map();
+  let isFastForwarded = false;
+
+  // simulate game
+  while (n > 0) {
+    if (!isFastForwarded) {
+      // check if we can fast forward
+      if (cache.has(cells)) {
+        n = n % cache.get(cells) - n;
+        isFastForwarded = true;
+      } else {
+        cache.set(cells, n);
+      }
+    }
+
+    if (N > 0) {
+      n--;
+      updateCells(cells);
+    }
+
+  }
+
+  return cells;
 };
 
+var updateCells = function(cells) {
+  cells[0] = 0;
+  cells[cells.length - 1] = 0;
+  for (let i = 1; i < cells.length - 1; i++) {
+    if (cells[i - 1] === cells[i + 1]) {
+      cells[i] = 1;
+    } else {
+      cells[i] = 0;
+    }
+  }
+};
